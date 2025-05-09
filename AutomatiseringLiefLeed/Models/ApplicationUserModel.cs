b2c89ffd578680252e7772134e7e5a2e0f69a57b
HollebeekW <hollebeekw@gmail.com>
2025-05-09 16:17:10 +0200

    Created models

---
+++ AutomatiseringLiefLeed/Models/ApplicationUserModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutomatiseringLiefLeed.Models
+{
+    public class ApplicationUserModel : IdentityUser
+    {
+        [Required, ForeignKey("DepartmentModel")]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
+
+        [Required]
+        public DateOnly DateOfEmployment { get; set; }
+
+        public DateOnly? DateOfMarriage { get; set; }
+
+        [Required]
+        public bool IsSick { get; set; }
+
+        public DateOnly? DateOfSickness { get; set; }
+    }
+}
