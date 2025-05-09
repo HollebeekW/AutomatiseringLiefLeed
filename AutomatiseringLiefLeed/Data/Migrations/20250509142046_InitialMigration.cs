940f5e1d03585caa3939a648bc3c16b7b9a9da41
HollebeekW <hollebeekw@gmail.com>
2025-05-09 16:22:22 +0200

    Added Initial Migration

---
+++ AutomatiseringLiefLeed/Data/Migrations/20250509142046_InitialMigration.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutomatiseringLiefLeed.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfEmployment",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfMarriage",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfSickness",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSick",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationModelSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    RecipientId = table.Column<int>(type: "int", nullable: false),
+                    ReasonId = table.Column<int>(type: "int", nullable: false),
+                    DateOfApplication = table.Column<DateOnly>(type: "date", nullable: false),
+                    DateOfIssue = table.Column<DateOnly>(type: "date", nullable: false),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationModelSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DepartmentModelSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentModelSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReasonModelSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReasonModelSet", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationModelSet");
+
+            migrationBuilder.DropTable(
+                name: "DepartmentModelSet");
+
+            migrationBuilder.DropTable(
+                name: "ReasonModelSet");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfEmployment",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfMarriage",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfSickness",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsSick",
+                table: "AspNetUsers");
+        }
+    }
+}
