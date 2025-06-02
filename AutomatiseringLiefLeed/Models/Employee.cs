c4c28828207718016aabe505a97aebb665fcc615
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2025-06-02 08:50:14 +0200

    changed reason model and added employee and request models

---
+++ AutomatiseringLiefLeed/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutomatiseringLiefLeed.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Medewerker { get; set; }
+
+        public string Roepnaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public string EmailWerk { get; set; }
+
+        public DateOnly GeboorteDatum { get; set; }
+
+        public DateOnly AOWDatum { get; set; }
+
+        public DateOnly InDienstIVMDienstJaren { get; set; }
+    }
+}
---
+++ AutomatiseringLiefLeed/Models/Request.cs
@@ -0,0 +1,38 @@
+﻿using AutomatiseringLiefLeed.Services;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutomatiseringLiefLeed.Models
+{
+    public class Request
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int SenderId { get; set; }
+
+        [ForeignKey(nameof(SenderId))]
+        public Employee Sender { get; set; }
+
+        [Required]
+        public int RecipientId { get; set; }
+
+        [ForeignKey(nameof(RecipientId))]
+        public Employee Recipient { get; set; }
+
+        [Required]
+        public int ReasonId { get; set; }
+
+        [ForeignKey(nameof(ReasonId))]
+        public Reason Reason { get; set; }
+
+        [Required]
+        public DateOnly DateOfApplication { get; set; }
+
+        [Required]
+        public DateOnly DateOfIssue { get; set; }
+
+        public string Note { get; set; }
+    }
+}
