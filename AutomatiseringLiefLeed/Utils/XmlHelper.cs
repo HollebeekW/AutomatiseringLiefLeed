5cbfdce286762b3aabedc2b69c3eefd7212446bc
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2025-06-02 09:16:13 +0200

    Added services and DTO to deserialise provided xml to employee table

---
+++ AutomatiseringLiefLeed/Utils/XmlHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Serialization;
+using AutomatiseringLiefLeed.DTOs;
+
+namespace AutomatiseringLiefLeed.Utils
+{
+    public class XmlHelper
+    {
+        public static AfasGetConnector DeserialiseEmployees(string xml)
+        {
+            //try to deserialise xml file
+            try
+            {
+                var serialiser = new XmlSerializer(typeof(AfasGetConnector));
+                using var reader = new StringReader(xml);
+                return (AfasGetConnector)serialiser.Deserialize(reader);
+            }
+
+            //return error message if it fails to deserialise
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
