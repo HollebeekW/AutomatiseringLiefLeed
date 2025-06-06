980c27693dcb37a0e19e853b0fe56dbb19ca377d
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2025-06-06 11:21:29 +0200

    set up email services and config

---
+++ AutomatiseringLiefLeed/Services/Email/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Options;
+
+namespace AutomatiseringLiefLeed.Services.Email
+{
+    public interface IEmailService
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpSettings _settings;
+
+        public EmailService(IOptions<SmtpSettings> smtpOptions)
+        {
+            _settings = smtpOptions.Value;
+        }
+
+        public void SendEmail(string to, string subject, string body)
+        {
+            var message = new MailMessage(_settings.FromEmail, to, subject, body);
+
+            using var client = new SmtpClient(_settings.Host, _settings.Port)
+            {
+                Credentials = new NetworkCredential(_settings.Username, _settings.Password),
+                EnableSsl = _settings.EnableSsl,
+            };
+
+            client.Send(message);
+        }
+    }
+
+}
