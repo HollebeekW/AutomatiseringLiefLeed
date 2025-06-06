a09afcd970c60da85aefadf9f29537a144c8eb8d
HollebeekW <124771925+HollebeekW@users.noreply.github.com>
2025-06-06 10:09:26 +0200

    Fixed roles, only allowing logged in admin users to see navigation tabs to reasons, admin and application pages, and fixed authentication access to reasons page

--- AutomatiseringLiefLeed/Controllers/ReasonsController.cs
+++ AutomatiseringLiefLeed/Controllers/ReasonsController.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutomatiseringLiefLeed.Data;
+using AutomatiseringLiefLeed.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using AutomatiseringLiefLeed.Data;
-using AutomatiseringLiefLeed.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutomatiseringLiefLeed.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ReasonsController : Controller
     {
         private readonly ApplicationDbContext _context;
