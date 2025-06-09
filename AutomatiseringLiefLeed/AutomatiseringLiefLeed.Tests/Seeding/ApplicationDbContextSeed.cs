4ff6e931fb2816d44bae1eb182e5326d371d210c
Erkan21211 <E.aslantas@hotmail.com>
2025-06-09 17:55:56 +0200

    bring changes

--- AutomatiseringLiefLeed/AutomatiseringLiefLeed.Tests/Seeding/ApplicationDbContextSeed.cs
+++ AutomatiseringLiefLeed/AutomatiseringLiefLeed.Tests/Seeding/ApplicationDbContextSeed.cs
@@ -17,30 +17,30 @@ namespace AutomatiseringLiefLeed.AutomatiseringLiefLeed.Tests.Seeding
                 await userManager.CreateAsync(user, "Admin123!");
             }
 
-            if (!context.Applications.Any())
-            {
-                context.Applications.AddRange(
-                    new Application
-                    {
-                        SenderId = "1",
-                        RecipientId = "2",
-                        DateOfApplication = DateTime.Now,
-                        DateOfIssue = DateTime.Now.AddDays(7),
-                        IsAccepted = false,
-                        ReasonId = 1
-                    },
-                    new Application
-                    {
-                        SenderId = "2",
-                        RecipientId = "3",
-                        DateOfApplication = DateTime.Now,
-                        DateOfIssue = DateTime.Now.AddDays(14),
-                        IsAccepted = true,
-                        ReasonId = 2
-                    }
-                );
-                context.SaveChanges();
-            }
+            //if (!context.Applications.Any())
+            //{
+            //    context.Applications.AddRange(
+            //        new Application
+            //        {
+            //            SenderId = "1",
+            //            RecipientId = "2",
+            //            DateOfApplication = DateTime.Now,
+            //            DateOfIssue = DateTime.Now.AddDays(7),
+            //            IsAccepted = false,
+            //            ReasonId = 1
+            //        },
+            //        new Application
+            //        {
+            //            SenderId = "2",
+            //            RecipientId = "3",
+            //            DateOfApplication = DateTime.Now,
+            //            DateOfIssue = DateTime.Now.AddDays(14),
+            //            IsAccepted = true,
+            //            ReasonId = 2
+            //        }
+            //    );
+            //    context.SaveChanges();
+            //}
         }
     }
 }
---
+++ AutomatiseringLiefLeed/Migrations/20250605144712_ApplicationModelRelationFix.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutomatiseringLiefLeed.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationModelRelationFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAccepted",
+                table: "Applications",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfApplication",
+                table: "Applications",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_RecipientId",
+                table: "Applications",
+                column: "RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Applications_Employees_RecipientId",
+                table: "Applications",
+                column: "RecipientId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Applications_Employees_RecipientId",
+                table: "Applications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_RecipientId",
+                table: "Applications");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAccepted",
+                table: "Applications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfApplication",
+                table: "Applications",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
